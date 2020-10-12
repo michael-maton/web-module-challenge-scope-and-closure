7c189ccc841bf142eed1a73eec5fedb67eac8c8a
Michael Maton <mjmaton6@gmail.com>
2020-10-12 15:08:12 -0500

    scoreboard completed

--- index.js
+++ index.js
@@ -15,7 +15,7 @@
  * should return 'foofoo'.
 */
 function processFirstItem(stringList, callback) {
-  return callback(stringList[0])
+  return callback(stringList[0]);
 }
 
 // ⭐️ Example Challenge END ⭐️
@@ -27,10 +27,14 @@ function processFirstItem(stringList, callback) {
  * Study the code for counter1 and counter2. Answer the questions below.
  * 
  * 1. What is the difference between counter1 and counter2?
+ * counter1 remembers how many times you run it. It contains a closure?
  * 
  * 2. Which of the two uses a closure? How can you tell?
+ * counter1, it remembers how many times you call the function.
  * 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
+ * counter1 would be preferable if we need to remember a piece of information for the next time the function is called.
+ * counter2 would be more preferable if we need the function to just do what it's told over and over without remembering previous callings of it.
  *
 */
 
@@ -42,25 +46,50 @@ function counterMaker() {
   }
 }
 
-const counter1 = counterMaker();
 
-// counter2 code
+
+const counter1 = counterMaker();
+// console.log(counter1());
+// console.log(counter1());
+// console.log(counter1());
+// console.log(counter1());
+// console.log(counter1());
+// console.log(counter1());
+
+
 let count = 0;
 
 function counter2() {
   return count++;
 }
 
+// console.log(counter2);
+// console.log(counter2);
+// console.log(counter2);
+// console.log(counter2);
+// console.log(counter2);
+
+
 
 /* Task 2: inning() 
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
-
-    /*Code Here*/
-
+function inning(num){
+  // console.log(`Inning #${num}`);
+  return (Math.floor(Math.random()*3));
 }
+// console.log(inning());
+
+// function inning(numOfInnings){
+//   const rand = Math.floor(Math.random()*3);
+//   for (let i=1; i <= numOfInnings; i++){
+
+//   }
+
+
+// }
+// console.log(inning());
 
 /* Task 3: finalScore()
 
@@ -75,12 +104,35 @@ finalScore(inning, 9) might return:
 }
 
 */ 
+// function finalScore(inning, innNum){
+//   let home = 0;
+//   let away = 0;
+//   for (let i = 1; i <= innNum; i++){
+//     // console.log(inning);
+//     home = home + inning;
+//     away = away + inning;
+//     console.log(`Home: ${home}`);
+//   }
+//   return [`Home: ${home}`, `Away: ${away}`];
+// }
+// console.log(finalScore(inning(), 9));
 
-function finalScore(/*code Here*/){
 
-  /*Code Here*/
-
+function finalScore(inning, innNum){
+  let home = 0;
+  let away = 0;
+  for (let i,j = 1; i,j <= innNum; i,j++){
+    home = home + inning(i);
+    away = away + inning(j);
+    // console.log(`Home: ${home}`, `Away: ${away}`);
+    // var randNum = inning(i);
+    // home = home + randNum;
+    // console.log(randNum);
+    // console.log(`Home: ${home}`);
+  }
+  return [`Home: ${home}`, `Away: ${away}`];
 }
+console.log(finalScore(inning, 9));
 
 /* Task 4: 
 
@@ -103,8 +155,20 @@ and returns the score at each pont in the game, like so:
 Final Score: awayTeam - homeTeam */
 
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard(inning, num) {
+  let home = 0;
+  let away = 0;
+  for (let i,j = 1; i,j <= num; i,j++){
+    home = home + inning(i);
+    away = away + inning(j);
+    // console.log(`Home: ${home}`, `Away: ${away}`);
+    // var randNum = inning(i);
+    // home = home + randNum;
+    // console.log(randNum);
+    console.log(`Inning #${j} Home: ${home}`, `Away: ${away}`);
+  }
+  return [`Final Score: ${home} - ${away}`];
 }
 
 
+console.log(scoreboard(inning, 9));
